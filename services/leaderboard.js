d5ffbe4acd8dc741809c0ae3eb4e1e160b2cdc29
sarosi <sarosigabor@gmail.com>
2026-05-21 10:19:52 +0200

    Add supabase url and anon key

--- services/leaderboard.js
+++ services/leaderboard.js
@@ -1,6 +1,6 @@
-const SUPABASE_URL = "YOUR_SUPABASE_URL";
+const SUPABASE_URL = "https://cwbhedgrwpbzsifkjnft.supabase.co/rest/v1/";
 
-const SUPABASE_KEY = "YOUR_ANON_KEY";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN3YmhlZGdyd3BienNpZmtqbmZ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkzMDM4MDQsImV4cCI6MjA5NDg3OTgwNH0.DSRSI3PKUAcKVl3zdr6Kg3eDIydHfNFxgfnWzCFXBDE";
 
 const headers = {
   apikey: SUPABASE_KEY,
